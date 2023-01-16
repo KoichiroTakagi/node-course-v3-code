315556eff083969f8b5d73d38023c3422bb45da7
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-01-16 21:24:45 +0900

    Done: Section4-15

--- self-notes-app/app.js
+++ self-notes-app/app.js
@@ -1,5 +1,12 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-console.log(getNotes())
-console.log(chalk.green.bold.inverse('Success!!'))
+const command = process.argv[2]
+
+console.log(process.argv)
+
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
