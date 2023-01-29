05b5e438f2589933e14709c3f7da5dd6e0f39fee
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-01-29 13:57:08 +0900

    Done: Section6-31

--- self-weather-app/app.js
+++ self-weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=8ca89b27a69f141d697bec252ba23f15&query=37.8267,122.4233'
 
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
