61ec27217862e6dfc4e0e03d81a9cd3e8faa7fd8
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-01-29 16:45:32 +0900

    Done: Section6-34

--- self-weather-app/app.js
+++ self-weather-app/app.js
@@ -6,3 +6,9 @@ request({ url: url, json: true }, (error, response) => {
     // console.log(response.body.current)
     console.log('%s. It is currently %d  degrees out. It feels like %d degrees out.', response.body.current.weather_descriptions[0], response.body.current.temperature, response.body.current.feelslike)
 })
+
+const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGFuYWtlbjEiLCJhIjoiY2xkaDFzZWFvMTJ2NTNxcHRtanVia3JyZCJ9.Guz5QzXUbVvX7i4_8RhB8w&limit=1'
+
+request({ url: url2, json: true }, (error, response) => {
+    console.log('latitude: %d, longitude: %d', response.body.features[0].center[1], response.body.features[0].center[0])
+})
