92a02b15207c98f50e3474e30a2e0228077a7bef
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-01-29 14:50:13 +0900

    Done: Section6-32

--- self-weather-app/app.js
+++ self-weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=8ca89b27a69f141d697bec252ba23f15&query=37.8267,122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=8ca89b27a69f141d697bec252ba23f15&query=37.8267,122.4233$units=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current)
+    console.log('%s. It is currently %d  degrees out. It feels like %d degrees out.', response.body.current.weather_descriptions[0], response.body.current.temperature, response.body.current.feelslike)
 })
