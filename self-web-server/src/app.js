dc6c28a3772f830e54133fd44254ff90fb7f5e0b
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-02-07 00:29:39 +0900

    Done: Section7-47

--- self-web-server/src/app.js
+++ self-web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Takagi'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Takagi'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'This is some helpful text.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
