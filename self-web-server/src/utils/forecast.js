b9b5ccd8257d34cef073d39ae91f029ddc8f6e73
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-02-11 23:36:46 +0900

    Done: Section8-55

---
+++ self-web-server/src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=8ca89b27a69f141d697bec252ba23f15&query=' + latitude +',' + longitude + '&units=m'
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another location.', undefined)
+        } else {
+            callback(undefined, {
+                description: body.current.weather_descriptions[0],
+                current_degree: body.current.temperature,
+                feels_like_degree: body.current.feelslike
+            })
+        }
+    })
+}
+
+module.exports = forecast
