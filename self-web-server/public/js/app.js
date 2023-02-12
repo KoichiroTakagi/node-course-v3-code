5615ad331a561d1314b3c02761094b12172bb97b
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-02-13 00:01:33 +0900

    Done: Section9-63

--- self-web-server/public/js/app.js
+++ self-web-server/public/js/app.js
@@ -1,18 +1,7 @@
-console.log('Client side javascript file is loaded!')
-
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-// messageOne.textContent = 'From JavaScript '
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
