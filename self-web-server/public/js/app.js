7f62f6908c8f8d1c636b9d7316a3566829517d0b
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-02-12 15:43:42 +0900

    Done: Section8-58

--- self-web-server/public/js/app.js
+++ self-web-server/public/js/app.js
@@ -6,13 +6,22 @@ fetch('https://puzzle.mead.io/puzzle').then((response) => {
     })
 })
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.forecast)
-            console.log(data.location)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.forecast)
+                console.log(data.location)
+            }
+        })
     })
 })
