f80a07b0fdd6398daf0baacc0cec6ab23e1632ba
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-02-12 15:13:03 +0900

    Done: Section8-57

--- self-web-server/public/js/app.js
+++ self-web-server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('https://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.forecast)
+            console.log(data.location)
+        }
+    })
+})
