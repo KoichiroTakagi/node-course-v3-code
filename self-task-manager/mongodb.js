c86d18127904fef5265954e28aa9fbccecf90247
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-02-16 01:41:12 +0900

    Done: Section10-75

---
+++ self-task-manager/mongodb.js
@@ -0,0 +1,22 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    console.log('Start!!')
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'takagi',
+        age: 33,
+    })
+    console.log('Done!!')
+})
