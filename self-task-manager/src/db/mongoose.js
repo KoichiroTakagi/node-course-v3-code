b28860914263f7018ff46830dc181e38b65ba0ad
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-02-19 22:54:29 +0900

    Done: Section11-85

--- self-task-manager/src/db/mongoose.js
+++ self-task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,16 +8,35 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: 'Takagi',
-    age: 'By'
+    name: '    asdf     ',
+    email: 'MT|YEMIAL@EMAI.oi            '
 })
 
 me.save().then(() => {
@@ -24,3 +44,23 @@ me.save().then(() => {
 }).catch((error) => {
     console.log('Error!', error)
 })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+// const task = new Task({
+//     description: 'One',
+//     completed: false
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
