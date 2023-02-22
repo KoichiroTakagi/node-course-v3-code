6f9304f34c4043b11554e117e006a959d103de3d
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-02-23 00:06:44 +0900

    Done: Section11-90

--- self-task-manager/src/db/mongoose.js
+++ self-task-manager/src/db/mongoose.js
@@ -4,16 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        required: false,
-        default: false
-    }
-})
