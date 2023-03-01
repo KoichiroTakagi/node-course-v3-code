a9919be7b6f16cb00b292906adc2b09f04b5b676
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-03-02 00:01:18 +0900

    Done: Section12-109

--- self-task-manager/src/index.js
+++ self-task-manager/src/index.js
@@ -14,9 +14,9 @@ const port = process.env.PORT || 3000
 //     }
 // })
 
-app.use((req, res, next) => {
-    res.status(503).send('Sorry, this site is currently under maintenance now.')
-})
+// app.use((req, res, next) => {
+//     res.status(503).send('Sorry, this site is currently under maintenance now.')
+// })
 
 app.use(express.json())
 app.use(userRouter)
