1bae88b7c65c02a424c6248ec5a9ec81daa66a40
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-03-06 22:12:55 +0900

    Done: Section15-132

--- self-task-manager/src/routers/user.js
+++ self-task-manager/src/routers/user.js
@@ -4,12 +4,14 @@ const auth = require('../middleware/auth')
 const router = new express.Router()
 const multer = require('multer')
 const sharp = require('sharp')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -79,6 +81,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendCancelationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
