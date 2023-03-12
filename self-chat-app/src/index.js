a34a2b02a097aac7e596395c243bc1e430643812
KoichiroTakagi <koichiro_takagi@earthbrain.com>
2023-03-12 17:45:53 +0900

    Done: Section17-154

--- self-chat-app/src/index.js
+++ self-chat-app/src/index.js
@@ -1,6 +1,21 @@
-const app = require('./app')
-const port = process.env.PORT || 3000
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
 
-app.listen(port, () => {
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
+
+server.listen(port, () => {
     console.log('Server is up to port ' + port)
 })
